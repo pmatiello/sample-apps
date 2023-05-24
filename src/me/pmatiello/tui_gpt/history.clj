b9d06ee2c81028b689a7fd14052fb092b0b34c97
Pedro Matiello <pedro@pmatiello.me>
2023-05-24 20:22:39 -0300

    Refactor history/compress.

--- src/me/pmatiello/tui_gpt/history.clj
+++ src/me/pmatiello/tui_gpt/history.clj
@@ -14,18 +14,19 @@
   :args (s/cat :history ::history :message ::message/message)
   :ret ::history)
 
+(defn ^:private with-acc-tokens
+  [history {:keys [tokens] :as each}]
+  (let [acc-tokens (-> history first :acc-tokens (or 0))
+        each       (assoc each :acc-tokens (+ acc-tokens tokens))]
+    (conj history each)))
+
 (defn compress
   [history size]
   (->> history
        reverse
-       (reduce
-         (fn [acc {:keys [tokens] :as each}]
-           (let [sum-tokens (-> acc first :sum-tokens (or 0))
-                 each       (assoc each :sum-tokens (+ sum-tokens tokens))]
-             (conj acc each)))
-         nil)
-       (drop-while #(-> % :sum-tokens (> size)))
-       (mapv #(dissoc % :sum-tokens))))
+       (reduce with-acc-tokens nil)
+       (drop-while #(-> % :acc-tokens (> size)))
+       (mapv #(dissoc % :acc-tokens))))
 
 (s/fdef compress
   :args (s/cat :history ::history :size integer?)
