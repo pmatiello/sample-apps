91e0924e6340a6f2b5ff6d7d8ae9bfd38ebefacf
Pedro Matiello <pedro@pmatiello.me>
2023-05-28 20:07:42 -0300

    Add web app for chatting with gpt.

---
+++ web-gpt/README.md
@@ -0,0 +1,23 @@
+# me.pmatiello/web-gpt
+
+GPT chat as a web app to exercise
+[me.pmatiello/openai-api](https://github.com/pmatiello/openai-api).
+
+**Notice:** This is not an official OpenAI project nor is it affiliated with OpenAI
+in any way.
+
+## Development
+
+Information for developing this project.
+
+### Running
+
+The following command will execute the application:
+
+```
+% clj -M:run
+```
+
+## Contribution Policy
+
+This software is open-source, but closed to contributions.
---
+++ web-gpt/src/me/pmatiello/sample_apps/web_gpt/app.clj
@@ -0,0 +1,77 @@
+(ns me.pmatiello.sample-apps.web-gpt.app
+  (:gen-class)
+  (:require [markdown.core :as markdown]
+            [me.pmatiello.sample-apps.gpt-core.history :as history]
+            [me.pmatiello.sample-apps.gpt-core.message :as message]
+            [me.pmatiello.sample-apps.gpt-core.openai-api :as openai-api]
+            [me.pmatiello.sample-apps.gpt-core.params :as params]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :as r.m.params]
+            [selmer.parser :as selmer]))
+
+(def ^:private api-config
+  (openai-api/config params/api-key))
+
+(def ^:private history
+  (atom []))
+
+(defn ^:private get-home
+  []
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (selmer/render-file "index.html" {})})
+
+(defn ^:private render-content
+  [{:keys [content] :as message}]
+  (assoc message :content (markdown/md-to-html-string content)))
+
+(defn ^:private get-history
+  []
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (selmer/render-file "history.html"
+                                {:history (->> (map render-content @history))})})
+
+(defn ^:private add-to-history
+  [message]
+  (swap! history conj message))
+
+(defn ^:private post-prompt
+  [params]
+  (let [prompt (get params "prompt")
+        prompt-tokens (openai-api/token-count prompt api-config)
+        prompt-msg (message/new "user" prompt prompt-tokens)
+        _ (add-to-history prompt-msg)
+        remaining-tokens (- params/max-tokens prompt-tokens)
+        compressed-history (history/compress @history remaining-tokens)
+        messages (history/append compressed-history prompt-msg)]
+
+    (future
+      (-> messages
+          (openai-api/chat api-config)
+          add-to-history))
+
+    {:status 200
+     :header {"Content-Type" "text/html"}
+     :body   (selmer/render-file "form.html" {})}))
+
+(def ^:private not-found
+  {:status  404
+   :headers {"Content-Type" "text/plain"}
+   :body    "Not Found"})
+
+(defn ^:private handler*
+  [{:keys [request-method uri params] :as request}]
+  (case [request-method uri]
+    [:get "/"] (get-home)
+    [:get "/history"] (get-history)
+    [:post "/prompt"] (post-prompt params)
+    not-found))
+
+(def handler
+  (r.m.params/wrap-params handler*))
+
+(defn -main
+  []
+  (selmer/cache-off!)
+  (jetty/run-jetty handler {:port 3000}))
